6dc999dfbc8fb4479ce38c4c6d9f4832ca9f7e50
Maira Medrano <mairamedrano28@gmail.com>
2023-10-05 15:50:01 -0400

    implement images and button

--- src/app/about/back/page.js
+++ src/app/about/back/page.js
@@ -1,15 +1,16 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+import Button from '@/app/buttons/button';
 
 export default function Page() {
 	const router = useRouter();
 
 	return (
-		<button
+		<Button
 			type='button'
 			onClick={() => router.push('/', { scroll: false })}>
 			Go back
-		</button>
+		</Button>
 	);
 }
